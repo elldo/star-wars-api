7596af2ad0646a03f4a5573daf58bab2c63e8fe6
elldo <elldo90@gmail.com>
2017-03-18 15:08:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function search(category, callback) {
     $.ajax({
         type: "GET",
-        url: "http://swapi.co/api/" + category,
+        url: "https://swapi.co/api/" + category,
         success: callback
     });
 }
